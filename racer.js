3e1d29ac54a1206eddc102b0199c69b9827d1a3c
George Francis <georgefrancis105@gmail.com>
2016-05-04 18:56:29 +1200

    changed raceCars to an object

--- racer.js
+++ racer.js
@@ -1,28 +1,37 @@
 
-var raceCar1 = document.getElementById("car1");
-var raceCar2 = document.getElementById("car-2");
-var raceCar3 = document.getElementById("car-3");
-var raceCar4 = document.getElementById("car-4");
+var raceCar = function (ID, keyNum) {
+  this.carElement = document.getElementById(ID);
+  this.position = 0;
+  this.speed = 0;
+  this.keyNum = keyNum;
+  this.moveCar = function(){
+    this.position += 1;
+    this.carElement.style.left = this.position + 'px';
+    console.log("Moved " + this.carElement.innerHTML)
+  }
+};
 
-var track1 = document.getElementById("track-1");
-var track2 = document.getElementById("track-2");
-var track3 = document.getElementById("track-3");
-var track4 = document.getElementById("track-4");
+// var raceCar1 = document.getElementById("car-1");
+// var raceCar2 = document.getElementById("car-2");
+// var raceCar3 = document.getElementById("car-3");
+// var raceCar4 = document.getElementById("car-4");
+var raceCars = [];
+raceCars[0] = new raceCar("car-1",65);
+raceCars[1] = new raceCar("car-2",68);
+raceCars[2] = new raceCar("car-3",71);
+raceCars[3] = new raceCar("car-4",74);
 
-var raceCar1Pos = 0;
 
-window.addEventListener("keypress", keyPressed);
+// var raceCar1Pos = 0;
+
+window.addEventListener("keyup", keyPressed);
 
 function keyPressed(event){
-  console.log(raceCar1.innerHTML);
-  console.log(track1);
-  console.log(event.which);
-  if(event.which == 97){
-    raceCar1Pos += 2;
-    raceCar1.style.left = raceCar1Pos + 'px';
-  }
-/*  switch(event.which){
-    case(97):
 
-  }*/
+  console.log(event.which);
+  for(i = 0; i < raceCars.length; i++){
+    if(raceCars[i].keyNum == event.which){
+      raceCars[i].moveCar();
+    }
+  }
 };
